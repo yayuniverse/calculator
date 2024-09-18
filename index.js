d05b7070432ffb4241acc2bdaf797d8f827d7409
yay <yaytrxsh@proton.me>
2024-09-18 01:29:35 +0100

    Added Main Display / Input Field

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const add = (num1, num2) => { num1 + num2 };
-const subtract = (num1, num2) => { num1 - num2 };
-const multiply = (num1, num2) => { num1 * num2 };
-const divide = (num1, num2) => { num1 / num2 };
+// Operator functions
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// Operate function
+function operate(num1, num2, operator) {
+  return operator(num1, num2)
+}
+
+const num1 = null;
+const num2 = null;
+const operator = null;
