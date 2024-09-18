e9e3a929b0e3396310b31a6ce3ea84ce5547edc5
yay <yaytrxsh@proton.me>
2024-09-18 16:11:43 +0100

    Updated Calculator Feature To Work With Copy-Pasted Inputs

--- index.js
+++ index.js
@@ -16,35 +16,42 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-// Operate function
-function operate() {
-  console.log(operator(num1, num2));
-}
-
-function inputFormatter() {
-  output = displayField.value.split(/([+\-*/])/);
-  num1 = Number(output[0]);
-  num2 = Number(output[output.length - 1]);
-}
-
 const displayField = document.querySelector("#displayField");
 
+function inputFormatter() {
+  output = displayField.value.split(/([+\-*/])/); //split by operator
+  num1 = Number(output[0]);
+  operator = output[1];
+  num2 = Number(output[2]);
+}
+
+// Operate function
+function operate() {
+  if (operator === "+") return add(num1, num2);
+  else if (operator === "-") return subtract(num1, num2);
+  else if (operator === "*") return multiply(num1, num2);
+  else if (operator === "/") return divide(num1, num2);
+  else return "Error: Invalid operator";
+}
+
 displayField.addEventListener("keypress", (event) => {
-  if (event.key === "+") operator = add;
-  else if (event.key === "-") operator = subtract;
-  else if (event.key === "*") operator = multiply;
-  else if (event.key === "/") operator = divide;
-  else if (event.key === "Enter") {
+  if (event.key === "Enter") {
     inputFormatter();
-    operate();
+    displayField.value = operate();
   }
 });
 
 // The next two event listeners ensure the cursor always stays at the end of the input field
-displayField.addEventListener('input', function() {
-  displayField.setSelectionRange(displayField.value.length, displayField.value.length);
+displayField.addEventListener("input", function () {
+  displayField.setSelectionRange(
+    displayField.value.length,
+    displayField.value.length,
+  );
 });
 
-displayField.addEventListener('click', function() {
-  displayField.setSelectionRange(displayField.value.length, displayField.value.length);
+displayField.addEventListener("click", function () {
+  displayField.setSelectionRange(
+    displayField.value.length,
+    displayField.value.length,
+  );
 });
